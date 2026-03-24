3d96e30eb2cd19873a184db9372e709c57ad6f36
Soriano34 <soriano.ryan34@gmail.com>
2026-03-24 19:58:09 +0100

    Fin de la partie 2.5 prendreEtal

--- src/controleur/ControlTrouverEtalVendeur.java
+++ src/controleur/ControlTrouverEtalVendeur.java
@@ -1,5 +1,6 @@
 package controleur;
 
+import personnages.Gaulois;
 import villagegaulois.Etal;
 import villagegaulois.Village;
 
@@ -11,8 +12,12 @@ public class ControlTrouverEtalVendeur {
 	}
 
 	public Etal trouverEtalVendeur(String nomVendeur) {
-		//TODO a completer
-		Etal etal = null;
+		Gaulois lionelMessi = village.trouverHabitant(nomVendeur);
+		Etal etal=null;
+		if (lionelMessi!=null) {
+			etal = village.rechercherEtal(lionelMessi);
+		}
 		return etal;
 	}
 }
+//ON EST INTERDIT D'IMPORTER GAULOIS ?????
